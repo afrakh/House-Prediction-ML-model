341d9cfdea9f47030abab62a985b82438acf0231
AfraKhurram <160423001+afrakh@users.noreply.github.com>
2025-12-29 01:44:13 +0500

    Add files via upload

---
+++ House-Prediction/server/server.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify, render_template
+import json
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load model & columns
+with open('../models/columns.json', 'r') as f:
+    data_columns = json.load(f)['data_columns']
+
+with open('../models/bangalore_home_prices_model.pickle', 'rb') as f:
+    model = pickle.load(f)
+
+locations = data_columns[3:]
+
+def get_location_index(location):
+    try:
+        return data_columns.index(location)
+    except:
+        return -1
+
+@app.route('/')
+def home():
+    return render_template('app.html')
+
+@app.route('/get_locations')
+def get_locations():
+    return jsonify({'locations': locations})
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    data = request.get_json()
+
+    total_sqft = float(data['total_sqft'])
+    bhk = int(data['bhk'])
+    bath = int(data['bath'])
+    location = data['location']
+
+    # Prepare input array
+    x = np.zeros(len(data_columns))
+    x[0] = total_sqft
+    x[1] = bath
+    x[2] = bhk
+
+    loc_index = get_location_index(location)
+    if loc_index >= 0:
+        x[loc_index] = 1
+
+    # Predict
+    price = model.predict([x])[0]
+    price = max(0, round(price, 2))  # Clamp negative predictions
+
+    return jsonify({'estimated_price': price})
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
